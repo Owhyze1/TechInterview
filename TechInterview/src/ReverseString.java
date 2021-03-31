865e876eeacfd21cca6a818396cde535abf3e0f1
Tiffany <TiffanyJWise84@gmail.com>
2021-03-31 11:51:52 -0400

    reversing char array O(1) extra memory

---
+++ TechInterview/src/ReverseString.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class ReverseString {
+
+    private char[] characters;
+
+    /**
+     * Write a function that reverses a string. The input string is given as an array of characters s.
+     * Source: LeetCode
+     * @param s char array of characters to be reversed without allocating space for another array
+     */
+    public static void reverse(char[] s){
+
+        char swap;
+        int len = s.length;
+        int end;
+
+        for (int i = 0; i < len/2; i++){
+
+            end = len - 1 - i;
+            
+            swap = s[i];
+            s[i] = s[end];
+            s[end] = swap;
+        }
+    }
+
+    
+    /**
+     * Generate char array to reverse
+     * 
+     * @param size size of desired char array     
+     */
+    public static char[] generateInput(int size){
+        
+        if ( size < 1 )
+            System.out.println("Invalid input");
+
+        char[] output = new char[size];
+        int num;
+
+        for (int i = 0; i < size; i ++){
+            num = new Random().nextInt(26) + (int)'a';
+
+            output[i] = (char) num;
+        }
+
+        return output;
+    }
+
+
+    /**
+     * Display contents of char array
+     */
+    public static void print(char[] letters){
+
+        for (char c : letters){
+            System.out.print(c + " ");
+        }
+        System.out.println();
+    }
+}
