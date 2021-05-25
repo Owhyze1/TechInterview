9555c0b55a4ddc065d232ab207e6493a4bd1d428
Tiffany <Owhyze1@gmail.com>
2021-05-25 19:36:23 -0400

    Reversing a string`

--- TechInterview/src/App.java
+++ TechInterview/src/App.java
@@ -3,5 +3,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         ReverseString.testingReverseOperation();
+
+        String test = "ReverseThisString ";
+
+        ReverseVowels.testVowelReverse(test);
     }
 }
